37e3cc01a8e8aebe20212e3e0076bea4eced9375
Matt Wellman <williamwellman98@gmail.com>
2021-01-12 02:35:01 -0500

    cleaned up profile AcheivementBanner a bit

---
+++ client/Screens/ProfileScreen/ProfileComponents/AcheivementBannerIconBlock.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faClock, faDumbbell, faBolt} from '@fortawesome/free-solid-svg-icons';
+
+interface AcheivementIconBlockProps {
+  icon: any;
+  textSubject: string;
+}
+
+const AcheivementIconBlock: React.FC<AcheivementIconBlockProps> = ({
+  icon,
+  textSubject,
+}) => {
+  return (
+    <View style={styles.iconColumn}>
+      <View style={styles.iconHalo}>
+        <FontAwesomeIcon icon={icon} size={30} color={'#0896a3'} />
+      </View>
+      <Text style={styles.detailTitle}>{textSubject}</Text>
+      <View>
+        <Text>3 hrs 12 min</Text>
+        <Text></Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  badgeRow: {
+    borderTopColor: 'grey',
+    borderTopWidth: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    height: 130,
+    width: '100%',
+    justifyContent: 'space-evenly',
+    padding: 1,
+  },
+  iconColumn: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    height: 130,
+    justifyContent: 'center',
+    padding: 5,
+  },
+  iconHalo: {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: '#83c0c9',
+    borderRadius: 30,
+    height: 60,
+    width: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+  },
+  detailTitle: {
+    margin: 0,
+  },
+});
+export default AcheivementIconBlock;
